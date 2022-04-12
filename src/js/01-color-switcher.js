bab7d18fb2cec29f6abbf37a7c0ae4fc707a0568
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-04-12 18:46:53 +0300

    Первое задание выполнено

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const PROMPT_DELAY = 1000;
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
 
+let timerId = null;
+let stopBtnClicked = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeBodyBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startBtn.addEventListener("click", () => {
+    if (stopBtnClicked) {
+        return;
+    }
+
+    timerId = setInterval(changeBodyBackgroundColor, PROMPT_DELAY);
+    console.log(timerId);
+    stopBtnClicked = true;
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    stopBtnClicked = false;
+});
